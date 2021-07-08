c1768160ec4d9fab9ee6d9e32f2cbfb86393f48b
KrutikGadhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2021-07-08 16:25:40 +0530

    added technologies and githublink btn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { Typography, Container, TextField, Grid, Paper, Button, Tabs, Tab, AppBar, Box, Fab } from '@material-ui/core';
+import { Typography, TextField, Grid, Paper, Button, Tabs, Tab, AppBar, Box, Fab, OutlinedInput, InputLabel, MenuItem, FormControl, Chip, Select } from '@material-ui/core';
+import { useTheme } from "@material-ui/core/styles";
 import SaveIcon from '@material-ui/icons/Save';
 import LinkOutlinedIcon from '@material-ui/icons/LinkOutlined';
 import ReplayOutlinedIcon from '@material-ui/icons/ReplayOutlined';
@@ -8,16 +9,53 @@ import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Loader from './loader.png';
 
+import HTML from './img/html.svg';
+import CSS from './img/css.svg';
+import JS from './img/js.svg';
+import REACT from './img/react.svg';
+import EXPRESS from './img/express.png';
+import CCPP from './img/CC++.png';
+import NODE from './img/nodejs.svg';
+import MONGODB from './img/mongodb.png';
+import PYTHON from './img/python.svg';
+import MATERIALUI from './img/materialui.svg';
+
 toast.configure()
 
+const names = [
+  "HTML5",
+  "CSS3",
+  "Java Script",
+  "React JS",
+  "Node JS",
+  "Express JS",
+  "MongoDB",
+  "C/C++",
+  "Python",
+  "Material UI"
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium
+  };
+}
+const url = 'https://portofolio-helper-server.herokuapp.com/save';
+// const url = 'http://localhost:5000/save';
 function App() {
   const [name, setName] = useState('');
   const [desc, setDesc] = useState('');
   const [link, setLink] = useState('');
+  const [githubLink, setGithubLink] = useState('');
+  const [technologies, setTechnologies] = useState([]);
   const [imgUrl, setImgUrl] = useState('https://via.placeholder.com/720x480');
   const [isUploading, setIsUploading] = useState(false);
   const [value, setValue] = useState(0);
   const [projects, setProjects] = useState([])
+  const theme = useTheme();
 
   useEffect(() => {
     fetch('https://portofolio-helper-server.herokuapp.com/getproject', {
@@ -30,7 +68,7 @@ function App() {
       })
       .catch((err) => {
         console.log(err)
-        const message = 'No Response from the Server'
+        let message = 'No Response from the Server'
         if (err.response) {
           message = err.response.message
         }
@@ -49,7 +87,7 @@ function App() {
       })
       .catch((err) => {
         console.log(err)
-        const message = 'No Response from the Server'
+        let message = 'No Response from the Server'
         if (err.response) {
           message = err.response.message
         }
@@ -58,7 +96,7 @@ function App() {
   }
 
   const handleSubmit = () => {
-    fetch('https://portofolio-helper-server.herokuapp.com/save', {
+    fetch(url, {
       method: 'post',
       headers: {
         "Content-Type": "application/json"
@@ -68,7 +106,9 @@ function App() {
         projectName: name,
         projectDescription: desc,
         projectImage: imgUrl,
-        projectLink: link
+        projectLink: link,
+        githubLink,
+        technologies,
       })
     })
       .then((res) => res.json())
@@ -85,6 +125,37 @@ function App() {
   const handleValue = (e, val) => {
     setValue(val);
   }
+
+  const techImgs = (tech) => {
+    switch (tech) {
+      case "HTML5":
+        return HTML;
+      case "CSS3":
+        return CSS;
+      case "Java Script":
+        return JS;
+      case "React JS":
+        return REACT;
+      case "Node JS":
+        return NODE;
+      case "MongoDB":
+        return MONGODB;
+      case "Express JS":
+        return EXPRESS;
+      case "C/C++":
+        return CCPP;
+      case "Python":
+        return PYTHON;
+      case "Material UI":
+        return MATERIALUI;
+      default:
+        break;
+    }
+  }
+
+  const handleChange = (event) => {
+    setTechnologies(event.target.value);
+  };
   return (
     <>
       <AppBar position="sticky">
@@ -132,6 +203,41 @@ function App() {
                     value={link}
                     onChange={(e) => setLink(e.target.value)}
                   />
+                </Grid>
+                <Grid style={{ marginBottom: '0.5em' }} item>
+                  <TextField
+                    fullWidth
+                    variant='outlined'
+                    label="Github Link"
+                    value={githubLink}
+                    onChange={(e) => setGithubLink(e.target.value)}
+                  />
+                </Grid>
+                <Grid style={{ marginBottom: '0.5em' }} item>
+                  <FormControl variant="outlined" fullWidth>
+                    <InputLabel id="technologies">Technologies Used</InputLabel>
+                    <Select
+                      labelId="technologies"
+                      multiple
+                      value={technologies}
+                      onChange={handleChange}
+                      input={<OutlinedInput label="Technologies Used" id="select-multiple-chip" />}
+                      renderValue={(selected) => (
+                        <div>
+                          {selected.map((value) => (
+                            <Chip key={value} label={value} />
+                          ))}
+                        </div>
+                      )}
+                    // MenuProps={MenuProps}
+                    >
+                      {names.map((name) => (
+                        <MenuItem key={name} value={name} style={getStyles(name, technologies, theme)}>
+                          {name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
                 </Grid>
                 <Grid item>
                   <Button
@@ -221,6 +327,18 @@ function App() {
                     <Grid style={{ textAlign: 'justify' }} item lg={6} md={6} sm={12} xs={12}>
                       <Typography style={{ fontWeight: '600' }} variant="h4" gutterBottom>{proj.projectName}</Typography>
                       <Typography variant="subtitle1" gutterBottom>{proj.projectDescription}</Typography>
+                      <Typography variant="h6" gutterBottom>Technologies Used:</Typography>
+                      <div className="techgrid">
+                        {
+                          proj.technologies.map((tech, index) => {
+                            return (
+                              <Box style={{ margin: '0 0.5em' }} gutterBottom key={index}>
+                                <img width="50px" src={techImgs(tech)} alt={tech} />
+                              </Box>
+                            )
+                          })
+                        }
+                      </div>
                       <Button
                         variant="contained"
                         color="primary"
@@ -229,6 +347,16 @@ function App() {
                         target="blank"
                       >
                         Live Project
+                      </Button>
+                      <Button
+                        style={{ marginLeft: '2em' }}
+                        variant="contained"
+                        color="secondary"
+                        endIcon={<LinkOutlinedIcon />}
+                        href={proj.githubLink}
+                        target="blank"
+                      >
+                        Github Link
                       </Button>
                     </Grid>
                     <Grid item lg={6} md={6} sm={12} xs={12}>
@@ -251,6 +379,18 @@ function App() {
                     <Grid style={{ testAlign: 'justify' }} item lg={6} md={6} sm={12} xs={12}>
                       <Typography style={{ fontWeight: '600' }} variant="h4" gutterBottom>{proj.projectName}</Typography>
                       <Typography variant="subtitle1" gutterBottom>{proj.projectDescription}</Typography>
+                      <Typography variant="h6" gutterBottom>Technologies Used:</Typography>
+                      <div className="techgrid">
+                        {
+                          proj.technologies.map((tech, index) => {
+                            return (
+                              <Box style={{ margin: '0 0.5em' }} gutterBottom key={index}>
+                                <img width="50px" src={techImgs(tech)} alt={tech} />
+                              </Box>
+                            )
+                          })
+                        }
+                      </div>
                       <Button
                         variant="contained"
                         color="primary"
@@ -259,6 +399,16 @@ function App() {
                         target="blank"
                       >
                         Live Project
+                      </Button>
+                      <Button
+                        style={{ marginLeft: '2em' }}
+                        variant="contained"
+                        color="secondary"
+                        endIcon={<LinkOutlinedIcon />}
+                        href={proj.githubLink}
+                        target="blank"
+                      >
+                        Github Link
                       </Button>
                     </Grid>
                   </Grid>
